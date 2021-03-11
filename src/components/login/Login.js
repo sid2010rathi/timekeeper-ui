bc1d967d2159a6803ad428d8bbaa90c6855e9a4c
Rakshit Chhodavadia <43170545+rakshit33@users.noreply.github.com>
2021-03-11 16:26:14 -0500

    Dashboard and Login Page Added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,80 +1,146 @@
-import { Component } from "react";
-import React from 'react';
-import { Container, Row, Col, Form, Button, Badge } from 'react-bootstrap';
+import React, {useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import { validateEmail, validatePassword } from '../../utility/validation';
-import { login } from '../../services/api'
+import { login } from '../../services/api';
 
-class Login extends Component {
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        TimeKeeper
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
-    constructor() {
-        super()
-        this.state = {
-            email: '',
-            password: '',
-            rememberMe: ''
-        };
-        this.onInputchange = this.onInputchange.bind(this);
-        this.onSubmitForm = this.onSubmitForm.bind(this);
-        localStorage.removeItem("username")
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Login() {
+
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [remember, setRemember] = useState();
+
+  const classes = useStyles();
+
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+    let data = {};
+    //validate inputs
+    if(validateEmail(email)) {
+        data.email = email;
     }
 
-    onInputchange(event) {
-        this.setState({
-          [event.target.name]: event.target.value
-        });
+    if(validatePassword(password)) {
+        data.password = password;
     }
-
-    onSubmitForm() {
-        let data = {};
-        //validate inputs
-        if(validateEmail(this.state.email)) {
-            data.email = this.state.email;
-        }
-
-        if(validatePassword(this.state.password)) {
-            data.password = this.state.password;
-        }
-
-        if(Object.keys(data).length > 1) {
-            login(data);
-        }
-    }
-
-    render() {
-        return(
-            <Container>
-                <Row>
-                    <Col />
-                    <Col xs={6}>
-                        <h3>
-                            <Badge variant="secondary">Login</Badge>
-                        </h3>
-                        <Form>
-                            <Form.Group>
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control type="email" placeholder="Enter email"
-                                 name="email" value={this.state.email} onChange={this.onInputchange} />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
-                            </Form.Group>
-
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password"
-                                 name="password" value={this.state.password} onChange={this.onInputchange} />
-                            </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Remember Me!" name="rememberMe" value={this.state.rememberMe} onChange={this.onInputchange} />
-                            </Form.Group>
-                            <Button variant="primary" onClick={this.onSubmitForm}>Submit</Button>
-                        </Form>
-                    </Col>
-                    <Col/>
-                </Row>
-            </Container>
-        )
+    console.log(data);
+    
+    if(Object.keys(data).length > 1) {
+        login(data);
     }
 }
 
-export default Login
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={onSubmitForm}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={event => setEmail(event.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={event => setPassword(event.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+            onChange={event => setRemember(event.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
